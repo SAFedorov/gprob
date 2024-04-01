Split the elementary operations in a separate module and made the tests pass with a temporary fix for broadcasting

plaingaussian/normal.py
@@ -1,152 +1,13 @@
-import itertools
-
 import numpy as np
 from numpy.linalg import LinAlgError
 
+from .elementary import (Elementary, add_maps, complete_maps, 
+                         join_maps, u_join_maps)
 from .func import logp
 
 # TODO: sampling would be very slow for large-aspect-ratio variables [[a0, a1, a2, a3, .. an]] for n >> 1
 # TODO: remove normal-normal arithmetic operations
-# TODO: clean up repr
 # TODO: add power operator
-
-
-class Elementary:
-
-    id_counter = itertools.count()
-
-    @staticmethod
-    def create(n: int):
-        return {next(Elementary.id_counter): i for i in range(n)}
-    
-    @staticmethod
-    def union(iids1: dict, iids2: dict):
-        """Ordered union of two dictionaries of iids."""
-
-        diff = set(iids2) - set(iids1)   
-        offs = len(iids1)
-
-        union_iids = iids1.copy()
-        union_iids.update({xi: (offs + i) for i, xi in enumerate(diff)}) 
-
-        return union_iids
-    
-    @staticmethod
-    def uunion(*args):
-        """Unordered union of multiple dictionaries of elementary variables."""
-        s = set().union(*args)
-        return {k: i for i, k in enumerate(s)}
-    
-    @staticmethod
-    def longer_first(op1, op2):
-        (_, iids1), (_, iids2) = op1, op2
-
-        if len(iids1) >= len(iids2):
-            return op1, op2, False
-        
-        return op2, op1, True
-
-    @staticmethod
-    def complete_maps(op1, op2):
-
-        (a1, iids1), (a2, iids2) = op1, op2
-
-        if iids1 is iids2:
-            return a1, a2, iids1
-
-        (a1, iids1), (a2, iids2), swapped = Elementary.longer_first(op1, op2)
-            
-        union_iids = Elementary.union(iids1, iids2)
-        a1_ = Elementary.pad_map(a1, len(union_iids))
-        a2_ = Elementary.extend_map(a2, iids2, union_iids)
-
-        if swapped:
-            a1_, a2_ = a2_, a1_
-
-        return a1_, a2_, union_iids
-    
-    @staticmethod
-    def add(op1, op2):
-
-        (a1, iids1), (a2, iids2) = op1, op2
-
-        if iids1 is iids2:
-            return a1 + a2, iids1
-
-        (a1, iids1), (a2, iids2), _ = Elementary.longer_first(op1, op2)
-            
-        union_iids = Elementary.union(iids1, iids2)
-        sum_a = Elementary.pad_map(a1, len(union_iids))
-
-        idx = [union_iids[k] for k in iids2]
-        sum_a[idx] += a2
-
-        return sum_a, union_iids
-    
-    @staticmethod
-    def pad_map(a, new_len):
-
-        len_ = a.shape[0]
-        new_shape = (new_len, *a.shape[1:])
-        new_a = np.zeros(new_shape)
-        new_a[:len_] = a
-
-        return new_a
-
-    @staticmethod
-    def extend_map(a, iids: dict, new_iids: dict):
-
-        new_shape = (len(new_iids), *a.shape[1:])
-        new_a = np.zeros(new_shape)
-        idx = [new_iids[k] for k in iids]
-        new_a[idx] = a
-
-        return new_a
-    
-    @staticmethod
-    def join_maps(op1, op2):
-
-        # Only works for 2D maps. Preserves the iids order.
-
-        (a1, iids1), (a2, iids2) = op1, op2
-
-        if iids1 is iids2:
-            return a1 + a2, iids1
-
-        (a1, iids1), (a2, iids2), swapped = Elementary.longer_first(op1, op2)
-            
-        union_iids = Elementary.union(iids1, iids2)
-        l1, l2 = a1.shape[1], a2.shape[1]
-        cat_a = np.zeros((len(union_iids), l1 + l2))
-
-        idx = [union_iids[k] for k in iids2]
-
-        if swapped:
-            cat_a[:len(iids1), l2:] = a1
-            cat_a[idx, :l2] = a2
-        else:
-            cat_a[:len(iids1), :l1] = a1
-            cat_a[idx, l1:] = a2
-
-        return cat_a, union_iids
-    
-    @staticmethod
-    def ujoin_maps(ops):
-        # ops is a sequence ((a1, iids1), (a2, iids2), ...), where `a`s are 
-        # strictly two-dimensional matrices.  
-
-        union_iids = Elementary.uunion(*[iids for _, iids in ops])
-
-        dims = [a.shape[1] for a, _ in ops]
-        cat_a = np.zeros((len(union_iids), sum(dims)))
-        n1 = 0
-        for i, (a, iids) in enumerate(ops):
-            n2 = n1 + dims[i]
-            idx = [union_iids[k] for k in iids]
-            cat_a[idx, n1: n2] = a
-            n1 = n2
-
-        return cat_a, union_iids
 
 
 class ConditionError(Exception):
@@ -201,7 +62,7 @@ class Normal:
 
     def __add__(self, other):
         if isinstance(other, Normal):
-            a, iids = Elementary.add((self.a, self.iids), (other.a, other.iids))
+            a, iids = add_maps((self.a, self.iids), (other.a, other.iids))
             return Normal(a, self.b + other.b, iids)
         
         return Normal(self.a, self.b + other, self.iids)
@@ -211,7 +72,7 @@ class Normal:
     
     def __sub__(self, other):
         if isinstance(other, Normal):
-            a, iids = Elementary.add((self.a, self.iids), (-other.a, other.iids))
+            a, iids = add_maps((self.a, self.iids), (-other.a, other.iids))
             return Normal(a, self.b - other.b, iids)
         
         return Normal(self.a, self.b - other, self.iids)
@@ -224,13 +85,16 @@ class Normal:
             # Linearized product  x * y = <x><y> + <y>dx + <x>dy,
             # for  x = <x> + dx  and  y = <y> + dy.
 
-            a, iids = Elementary.add((self.a * other.b, self.iids),
-                                     (other.a * self.b, other.iids))
+            a, iids = add_maps((self.a * other.b, self.iids),
+                               (other.a * self.b, other.iids))
             b = self.b * other.b
 
             return Normal(a, b, iids)
         
-        return Normal(self.a * other, self.b * other, self.iids)
+        b = self.b * other 
+        a = other * np.broadcast_to(self.a, (self.a.shape[0], *b.shape))
+        
+        return Normal(a, b, self.iids)
     
     def __rmul__(self, other):
         return self * other
@@ -240,13 +104,16 @@ class Normal:
             # Linearized fraction  x/y = <x>/<y> + dx/<y> - dy<x>/<y>^2,
             # for  x = <x> + dx  and  y = <y> + dy.
 
-            a, iids = Elementary.add((self.a / other.b, self.iids),
-                                     (other.a * (-self.b) / other.b**2, other.iids))
+            a, iids = add_maps((self.a / other.b, self.iids),
+                               (other.a * (-self.b) / other.b**2, other.iids))
             b = self.b / other.b
 
             return Normal(a, b, iids)
         
-        return Normal(self.a / other, self.b / other, self.iids)
+        b = self.b / other 
+        a = np.broadcast_to(self.a, (self.a.shape[0], *b.shape)) / other
+        
+        return Normal(a, b, self.iids)
     
     def __rtruediv__(self, other):
         # Linearized fraction  x/y = <x>/<y> - dy<x>/<y>^2,
@@ -295,8 +162,8 @@ class Normal:
         cond = join([k-v for k, v in observations.items()])
 
         # The calculation is performed on flattened arrays.
-        av, ac, union_iids = Elementary.complete_maps((self._a2d, self.iids), 
-                                                      (cond._a2d, cond.iids))
+        av, ac, union_iids = complete_maps((self._a2d, self.iids),
+                                           (cond._a2d, cond.iids))
         
         u, s, vh = np.linalg.svd(ac, compute_uv=True)
         tol = np.finfo(float).eps * np.max(ac.shape)
@@ -327,7 +194,7 @@ class Normal:
         new_b = self._b1d + m @ av
 
         # Shaping back
-        new_a = np.reshape(new_a, self.a.shape)
+        new_a = np.reshape(new_a, (av.shape[0], *self.a.shape[1:]))
         new_b = np.reshape(new_b, self.b.shape)
 
         return Normal(new_a, new_b, union_iids)
@@ -341,8 +208,7 @@ class Normal:
         if self.b.ndim > 1 or other.b.ndim > 1:
             raise ValueError("& operation is only applicable to 0- and 1-d arrays.")
         
-        cat_a, iids = Elementary.join_maps((self._a2d, self.iids), 
-                                           (other._a2d, other.iids))
+        cat_a, iids = join_maps((self._a2d, self.iids),(other._a2d, other.iids))
         cat_b = np.concatenate([self._b1d, other._b1d], axis=0)
 
         return Normal(cat_a, cat_b, iids)  
@@ -356,7 +222,7 @@ class Normal:
 
     def var(self):
         """Variance"""
-        var = np.einsum("ij, ij -> j", self._a2d, self._a2d).reshape()
+        var = np.einsum("ij, ij -> j", self._a2d, self._a2d)
         return var.reshape(self.shape)
     
     def cov(self):
@@ -399,7 +265,7 @@ def join(args):
 
     nvs = [asnormal(v) for v in args]
     ops = [(v._a2d, v.iids) for v in nvs]
-    a, iids = Elementary.ujoin_maps(ops)
+    a, iids = u_join_maps(ops)
     b = np.concatenate([np.array(v.b, ndmin=1) for v in nvs])
 
     return Normal(a, b, iids)
@@ -442,6 +308,8 @@ def normal(mu=0, sigmasq=1, size=1):
         # Vector of independent identically-distributed variables.
         return Normal(np.sqrt(sigmasq) * np.eye(size, size), mu * np.ones(size))
 
+    mu = np.broadcast_to(mu, (sigmasq.shape[0],))
+        
     try:
         atr = np.linalg.cholesky(sigmasq)
         return Normal(atr.T, mu)

tests/test_func.py
@@ -3,7 +3,7 @@ sys.path.append('..')  # Until there is a package structure.
 
 import pytest
 import numpy as np
-from plaingaussian.normal import N, join
+from plaingaussian.normal import normal, join
 from plaingaussian.func import logp, dlogp, d2logp, fisher
 
 from benchmarks.reffunc import logp as logp_
@@ -206,7 +206,7 @@ def test_logp_batch():
     nc = np.log(1/np.sqrt(2 * np.pi))  # Normalization constant.
     
     # Scalar variables
-    xi = N()
+    xi = normal()
     m, cov = xi.mean(), xi.cov()
     assert logp(0, m, cov) == nc
     assert (logp([0], m, cov) == np.array([nc])).all()
@@ -218,13 +218,13 @@ def test_logp_batch():
     with pytest.raises(ValueError):
         logp([[0, 1]], m, cov)
 
-    xi = N(0.9, 3.3)
+    xi = normal(0.9, 3.3)
     m, cov = xi.mean(), xi.cov()
     assert logp(2, m, cov) == (-(2-0.9)**2/(2 * 3.3)
                                      + np.log(1/np.sqrt(2 * np.pi * 3.3)))
 
     # Vector variables
-    xi = N(0.9, 3.3, size=2)
+    xi = normal(0.9, 3.3, size=2)
     m, cov = xi.mean(), xi.cov()
 
     res = (-(2-0.9)**2/(2 * 3.3)-(1-0.9)**2/(2 * 3.3) 
@@ -239,7 +239,7 @@ def test_logp_batch():
 
     assert (logp([[3.2, 1.2], [-1., -2.2]], m, cov) == np.array(res)).all()
 
-    xi = N(0.9, 3.3, size=2)
+    xi = normal(0.9, 3.3, size=2)
     m, cov = xi.mean(), xi.cov()
     with pytest.raises(ValueError):
         logp(0, m, cov)
@@ -253,7 +253,7 @@ def test_logp_batch():
     # Degenerate cases.
 
     # Deterministic variables.
-    xi = join(N(), 1)
+    xi = join([normal(), 1])
     m, cov = xi.mean(), xi.cov()
     assert logp([0, 1.1], m, cov) == float("-inf")
     assert (logp([[0, 1.1]], m, cov) == np.array([float("-inf")])).all()
@@ -262,8 +262,8 @@ def test_logp_batch():
             [nc, -(1.1)**2/(2) + nc, float("-inf")]).all()
     
     # Degenerate covariance matrix. 
-    xi1 = N()
-    xi2 = 0 * N()
+    xi1 = normal()
+    xi2 = 0 * normal()
     xi12 = xi1 & xi2
     m, cov = xi12.mean(), xi12.cov()
     assert logp([1.2, 0], m, cov) == -(1.2)**2/(2) + nc
@@ -275,14 +275,14 @@ def test_logp_batch():
     # TODO: add higher-dimensional examples
     
     # Integrals of the probability density
-    xi = N(0, 3.3)
+    xi = normal(0, 3.3)
     m, cov = xi.mean(), xi.cov()
     npt = 200000
     ls = np.linspace(-10, 10, npt)
     err = np.abs(1 - np.sum(np.exp(logp(ls, m, cov))) * (20)/ npt)
     assert err < 6e-6  # should be 5.03694e-06
 
-    xi = N(0, [[2.1, 0.5], [0.5, 1.3]])
+    xi = normal(0, [[2.1, 0.5], [0.5, 1.3]])
     m, cov = xi.mean(), xi.cov()
     npt = 1000
     ls = np.linspace(-7, 7, npt)

tests/test_inference.py
@@ -4,7 +4,7 @@ sys.path.append('..')  # Until there is a package structure.
 import pytest
 import numpy as np
 from external.Infer import Infer
-from plaingaussian.normal import N, join
+from plaingaussian.normal import normal, join, ConditionError
 
 
 def test_conditioning():
@@ -14,11 +14,11 @@ def test_conditioning():
     # Single constraint
 
     # This package
-    v1 = N(0.93, 1)
-    v2 = N(0, 1)
-    v3 = N(0, 1)
+    v1 = normal(0.93, 1)
+    v2 = normal(0, 1)
+    v3 = normal(0, 1)
 
-    vm = join(v1, v2, v3)
+    vm = join([v1, v2, v3])
     vc = vm | {v1 + 0.2*v2 + 0.4*v3: 1.4}
 
 
@@ -47,8 +47,8 @@ def test_conditioning():
     assert (np.abs(vc2.mean() - m2.b[:, 0]) < tol).all()
 
     # Incompatible conditions
-    with pytest.raises(RuntimeError):
-        join(v1, v2, v3) | {v2: 0, v3: 1, v1:0, v1+v2:1}
+    with pytest.raises(ConditionError):
+        join([v1, v2, v3]) | {v2: 0, v3: 1, v1:0, v1+v2:1}
 
 
 def test_linear_regression():
@@ -77,15 +77,15 @@ def test_linear_regression():
 
     # Comparison to a non-vectorized calculation using this package
 
-    mn = [N(0, 0.1) for _ in range(len(xs))]
+    mn = [normal(0, 0.1) for _ in range(len(xs))]
 
-    a = N(0, 10)
-    b = N(0, 10)
+    a = normal(0, 10)
+    b = normal(0, 10)
 
     cond = {f(x): y + n for (x, y, n) in zip(xs, ys, mn)}
 
     ab = (a & b) | cond
-    jointd = join(a, b, *mn) | cond
+    jointd = join([a, b, *mn]) | cond
 
     assert (np.abs(mfull.Sigma - jointd.cov()) < tol).all()
     assert (np.abs(mfull.b[:, 0] - jointd.mean()) < tol).all()
@@ -95,10 +95,10 @@ def test_linear_regression():
     # Comparison to a vectorized calculation using this package
 
     fv = a * xs + b
-    mnv = N(0, 0.1, size=len(xs))
+    mnv = normal(0, 0.1, size=len(xs))
 
     ab2 = (a & b) | {fv: ys + mnv}
-    jointd2 = join(a, b, mnv) | {fv: ys + mnv}
+    jointd2 = join([a, b, mnv]) | {fv: ys + mnv}
 
     assert (np.abs(mfull.Sigma - jointd2.cov()) < tol).all()
     assert (np.abs(mfull.b[:, 0] - jointd2.mean()) < tol).all()

tests/test_normal.py
@@ -3,49 +3,49 @@ sys.path.append('..')  # Until there is a package structure.
 
 import pytest
 import numpy as np
-from plaingaussian.normal import N, join, Normal
+from plaingaussian.normal import normal, join, Normal
 
 
-def test_N():
-    xi = N()
-    assert (xi.a == np.array([[1.]])).all()
-    assert (xi.b == np.array([0.])).all()
+def test_normal():
+    xi = normal()
+    assert (xi.a == np.array([1.])).all()
+    assert (xi.b == np.array(0.)).all()
 
-    xi = N(1.3, 4)
-    assert (xi.a == np.array([[2.]])).all()
-    assert (xi.b == np.array([1.3])).all()
+    xi = normal(1.3, 4)
+    assert (xi.a == np.array([2.])).all()
+    assert (xi.b == np.array(1.3)).all()
 
-    xi = N(0, 4, size=3)
+    xi = normal(0, 4, size=3)
     assert (xi.a == 2 * np.eye(3)).all()
     assert (xi.b == np.zeros(3)).all()
 
     with pytest.raises(ValueError):
-        N(0, -4)
+        normal(0, -4)
     
-    xi = N(0, 0)
-    assert (xi.a == np.array([[0.]])).all()
-    assert (xi.b == np.array([0.])).all()
+    xi = normal(0, 0)
+    assert (xi.a == np.array([0.])).all()
+    assert (xi.b == np.array(0.)).all()
 
     mu = [1.3, 2.5]
     cov = [[1., 0.], [0., 0.]]
-    xi = N(mu, cov)
-    assert (xi.a @ xi.a.T == np.array(cov)).all()
+    xi = normal(mu, cov)
+    assert (xi.cov() == np.array(cov)).all()
     assert (xi.b == mu).all()
 
     cov = [[2.1, 0.5], [0.5, 1.3]]
-    xi = N(0, cov)
+    xi = normal(0, cov)
     tol = 1e-14
-    assert (np.abs(xi.a @ xi.a.T - np.array(cov)) < tol).all()
+    assert (np.abs(xi.cov() - np.array(cov)) < tol).all()
     assert (xi.b == 0).all()
 
     # Covariance matrices with negative eigenvalues are not allowed
     with pytest.raises(ValueError):
-        N(0, [[0, 1], [1, 0]])
+        normal(0, [[0, 1], [1, 0]])
 
     # But covariance matrices with zero eigenvalues are.
     cov = [[1., 0.], [0., 0.]]
-    xi = N(0, cov)
-    assert (xi.a @ xi.a.T == np.array(cov)).all()
+    xi = normal(0, cov)
+    assert (xi.cov() == np.array(cov)).all()
 
 
 def test_logp():
@@ -54,20 +54,20 @@ def test_logp():
     nc = np.log(1/np.sqrt(2 * np.pi))  # Normalization constant.
     
     # Scalar variables
-    xi = N()
+    xi = normal()
     assert xi.logp(0) == nc
     assert xi.logp(1.1) == -1.1**2/2 + nc
 
-    xi = N(0.9, 3.3)
+    xi = normal(0.9, 3.3)
     assert xi.logp(2) == (-(2-0.9)**2/(2 * 3.3) 
                           + np.log(1/np.sqrt(2 * np.pi * 3.3)))
 
     # Vector variables
-    xi = N(0.9, 3.3, size=2)
+    xi = normal(0.9, 3.3, size=2)
     assert xi.logp([2, 1]) == (-(2-0.9)**2/(2 * 3.3)-(1-0.9)**2/(2 * 3.3) 
                                + 2 * np.log(1/np.sqrt(2 * np.pi * 3.3)))
 
-    xi = N(0.9, 3.3, size=2)
+    xi = normal(0.9, 3.3, size=2)
     with pytest.raises(ValueError):
         xi.logp(0)
     with pytest.raises(ValueError):
@@ -78,28 +78,28 @@ def test_logp():
     # Degenerate cases.
         
     # Zero scalar random variable.
-    assert (0 * N()).logp(0.) > float("-inf")
-    assert (0 * N()).logp(0.1) == float("-inf")
+    assert (0 * normal()).logp(0.) > float("-inf")
+    assert (0 * normal()).logp(0.1) == float("-inf")
 
     # Deterministic variables.
-    xi = join(N(), 1)
+    xi = join([normal(), 1])
     assert xi.logp([0, 1.1]) == float("-inf")
     assert xi.logp([0, 1.]) == nc
     assert xi.logp([10.2, 1.]) == -(10.2)**2/(2) + nc
     
     # Degenerate covariance matrix. 
-    xi1 = N()
-    xi2 = 0 * N()
+    xi1 = normal()
+    xi2 = 0 * normal()
     xi12 = xi1 & xi2
     assert xi12.logp([1.2, 0]) == -(1.2)**2/(2) + nc
     assert xi12.logp([1.2, 0.1]) == float("-inf")
 
 
 def test_len():
-    xi = N(0, 2, size=2)
+    xi = normal(0, 2, size=2)
     assert len(xi) == 2
 
-    xi = (N() & N() & N() & N())
+    xi = (normal() & normal() & normal() & normal())
     assert len(xi) == 4
 
 
@@ -107,14 +107,14 @@ def test_sample():
     
     # Checks the formats returned by sample()
     
-    v = N(0, 1)
+    v = normal(0, 1)
     s = v.sample()
     assert np.array(s).ndim == 0
 
     s = v.sample(3)
     assert s.shape == (3,)
 
-    v = N(0, 1, size=5)
+    v = normal(0, 1, size=5)
     s = v.sample()
     assert s.shape == (5,)
 
@@ -124,12 +124,9 @@ def test_sample():
 
 def test_join():
 
-    x = join(1)
-    assert isinstance(x, Normal) and len(x) == 1
-
-    v1 = N()
-    v2 = N()
-    v3 = N()
+    v1 = normal()
+    v2 = normal()
+    v3 = normal()
 
     vm = join(v1)  # single argument input
     assert vm == v1
@@ -138,21 +135,18 @@ def test_join():
     vm = join([v1])
     assert vm.a == v1.a and vm.b == v1.b and vm.iids == v1.iids
 
-    vm = join(v1, v2, v3)  # sequence input
-    assert isinstance(vm, Normal) and len(vm) == 3
-
     vm = join([v1, v2, v3])  # list input
     assert isinstance(vm, Normal) and len(vm) == 3
 
     vm = join((v1, v2, v3))  # tuple input
     assert isinstance(vm, Normal) and len(vm) == 3
 
-    v4 = N(0, 2, size=2)
-    vm = join(v1, v2, v4)
+    v4 = normal(0, 2, size=2)
+    vm = join([v1, v2, v4])
     assert isinstance(vm, Normal) and len(vm) == 4 and len(vm.iids) == 4
 
 
-def test_iids_ordering():
+def test_elementary_ordering():
     # Requires python >= 3.6
 
     def isordered(v):
@@ -160,9 +154,9 @@ def test_iids_ordering():
     
     # Scalars
 
-    v1 = N(0.1, 100)
-    v2 = N()
-    v3 = N()
+    v1 = normal(0.1, 100)
+    v2 = normal()
+    v3 = normal()
 
     v4 = v1 + 0.8 * v3 + 200.
     assert isordered(v4)
@@ -173,18 +167,18 @@ def test_iids_ordering():
     v5 = 0.02 + 13. * v5 + 20 * v1
     assert isordered(v5)
 
-    v5 = 0.02 + 13. * v5 + 0.4 * N() + 20 * v1
+    v5 = 0.02 + 13. * v5 + 0.4 * normal() + 20 * v1
     assert isordered(v5)
 
     nrv = 20
 
-    vl1 = [N() for _ in range(nrv)]
+    vl1 = [normal() for _ in range(nrv)]
     assert isordered(join(vl1))
 
-    vl2 = [N() for _ in range(nrv)]
+    vl2 = [normal() for _ in range(nrv)]
     assert isordered(join(vl2))
 
-    vl3 = [N() for _ in range(nrv // 2)]  # A shorter list.
+    vl3 = [normal() for _ in range(nrv // 2)]  # A shorter list.
     assert isordered(join(vl3))
 
     assert isordered(join(vl1 + vl2))
@@ -230,13 +224,13 @@ def test_iids_ordering():
 
     # Vectors
 
-    vl1 = [N(0.3, 12., size=3) for _ in range(nrv)]
+    vl1 = [normal(0.3, 12., size=3) for _ in range(nrv)]
     assert isordered(join(vl1))
 
-    vl2 = [N(size=3) for _ in range(nrv)]
+    vl2 = [normal(size=3) for _ in range(nrv)]
     assert isordered(join(vl2))
 
-    vl3 = [N(-3, 2., size=3) for _ in range(nrv // 2)]  # A shorter list.
+    vl3 = [normal(-3, 2., size=3) for _ in range(nrv // 2)]  # A shorter list.
     assert isordered(join(vl3))
 
     assert isordered(join(vl1 + vl2))
@@ -287,7 +281,7 @@ def test_operations():
                 and (np.abs(v1.b - v2.b) < tol).all())
     
     # 0d-1d
-    v = N(8, 1)
+    v = normal(8, 1)
     x_li = [2, 3]
     x_tu = tuple(x_li)
     x_ar = np.array(x_li)
@@ -312,7 +306,7 @@ def test_operations():
     assert isclose(v / x_tu, v / x_ar)
 
     # 1d-1d
-    v = N(0, 1, size=2)
+    v = normal(0, 1, size=2)
     x_li = [2, 3]
     x_tu = tuple(x_li)
     x_ar = np.array(x_li) 
@@ -333,19 +327,19 @@ def test_operations():
     assert isclose(v / x_li,  v / x_ar)
     assert isclose(v / x_tu, v / x_ar)
 
-    v1 = -N(0, 1, size=2) + np.array([2, 3])
-    v2 = np.array([2, 3]) - N(0, 1, size=2)
+    v1 = -normal(0, 1, size=2) + np.array([2, 3])
+    v2 = np.array([2, 3]) - normal(0, 1, size=2)
     assert isclose(v1, v2)
 
-    v1 = [2, 3] * N(0, 1, size=2)
-    v2 = np.array([2, 3]) * N(0, 1, size=2)
+    v1 = [2, 3] * normal(0, 1, size=2)
+    v2 = np.array([2, 3]) * normal(0, 1, size=2)
     assert isclose(v1, v2)
 
-    v1 = N(0, 1, size=2) * np.array([2, 3])
-    v2 = np.array([2, 3]) * N(0, 1, size=2)
+    v1 = normal(0, 1, size=2) * np.array([2, 3])
+    v2 = np.array([2, 3]) * normal(0, 1, size=2)
     assert isclose(v1, v2)
 
-    v1 = N(0, 1, size=2) * np.sqrt(2)
-    v2 = np.sqrt(2) * N(0, 1, size=2)
+    v1 = normal(0, 1, size=2) * np.sqrt(2)
+    v2 = np.sqrt(2) * normal(0, 1, size=2)
     assert isclose(v1, v2)
 