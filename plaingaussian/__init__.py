f4ef5f7d950f69d07bee3815efef22f0d26a5d6c
SAFedorov <fedorov.s.a@outlook.com>
2024-04-25 19:24:01 +0200

    Create __init__.py

---
+++ plaingaussian/__init__.py
@@ -0,0 +1,7 @@
+from plaingaussian.normal import (
+    normal, asnormal, stack, hstack, vstack, dstack, concatenate, 
+    split, hsplit, vsplit, dsplit, sum, cumsum, trace, diagonal, reshape, 
+    moveaxis, ravel, transpose, add, subtract, multiply, divide, power, 
+    einsum, dot, matmul, inner, outer, kron, tensordot, 
+    exp, exp2, log, log2, log10, sqrt, cbrt, sin, cos, tan, arcsin, arccos, 
+    arctan, sinh, cosh, tanh, arcsinh, arccosh, arctanh, conjugate, conj)
