Fixed the sign of infinity in dkl for degenerate arguments

gprob/func.py
@@ -82,7 +82,7 @@ def dkl(m1, cov1, m2, cov2):
         # Here, we use cholesky and not cho_factor because further we need
         # the full matrix, not only its lower triangular part.
     except LinAlgError:
-        return float("-inf")
+        return float("inf")
 
     s = sp.linalg.solve_triangular(ltr2, ltr1, check_finite=False, lower=True)
     strace = np.einsum("ij, ij -> ", s, s)

gprob/sparse.py
@@ -1078,7 +1078,7 @@ def dkl(x, y):
     try:
         ltr1 = np.linalg.cholesky(cov1)
     except LinAlgError:
-        return float("-inf")
+        return float("inf")
 
     s = np.linalg.solve(ltr2, ltr1)
     strace = np.einsum("ijk, ijk ->", s, s)

tests/test_normal.py
@@ -1421,10 +1421,10 @@ def test_dkl():
         x = random_normal(shape=sh, dtype=np.float64)
         y = random_normal(shape=sh, dtype=np.complex128)
 
-        assert np.isneginf(dkl(x, y))
+        assert np.isposinf(dkl(x, y))
 
         with pytest.raises(ValueError) as e:
-            assert np.isneginf(dkl(y, x))
+            assert np.isposinf(dkl(y, x))
 
         assert "degenerate" in get_message(e)
 
@@ -1460,7 +1460,7 @@ def test_dkl():
             assert "degenerate" in get_message(e)
 
             # first distribution is degenerate - numeric constant
-            assert np.isneginf(dkl(y, x))
+            assert np.isposinf(dkl(y, x))
 
             # second distribution is degenerate - covariance matrix
             x = normal(size=sh)
@@ -1476,7 +1476,7 @@ def test_dkl():
             assert "degenerate" in get_message(e)
 
             # first distribution is degenerate - covariance matrix
-            assert np.isneginf(dkl(y, x))
+            assert np.isposinf(dkl(y, x))
 
 
 def test_entropy():

tests/test_sparse.py
@@ -1058,10 +1058,10 @@ def test_dkl():
         x = iid(random_normal(shape=sh, dtype=np.float64), sparse_sz)
         y = iid(random_normal(shape=sh, dtype=np.complex128), sparse_sz)
 
-        assert np.isneginf(gp.dkl(x, y))
+        assert np.isposinf(gp.dkl(x, y))
 
         with pytest.raises(ValueError) as e:
-            assert np.isneginf(gp.dkl(y, x))
+            assert np.isposinf(gp.dkl(y, x))
 
         assert "degenerate" in get_message(e)
 
@@ -1085,7 +1085,7 @@ def test_dkl():
 
             assert "degenerate" in get_message(e)
 
-            assert np.isneginf(gp.dkl(y, x))
+            assert np.isposinf(gp.dkl(y, x))
 
             # One iaxis.
 